0c9837c66c2c73f6ac19871af9ed24e5ea8a1f90
Ivan Sidorenko <i.sidorenko@plarium.com>
2017-10-11 16:04:20 +0300

    add script to start project

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const helmet = require('helmet');
 
 // TODO path?
 const path = require('path');
 const app = express();
 
+// http://expressjs.com/uk/advanced/best-practice-security.html
+app.use(helmet());
 app.use(express.static(path.join(__dirname, './../frontend/build')));
 
 app.get('*', (req, res) => {
