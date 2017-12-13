f1ad7af38bd4f7a788057689ca3241800bf8d27c
Marina Yatsenko <mjacenk@gmail.com>
2017-12-13 11:45:49 +0200

    api server

--- backend/index.js
+++ backend/index.js
@@ -9,43 +9,84 @@ const mongoClient = require('mongodb').MongoClient;
 
 // http://expressjs.com/uk/advanced/best-practice-security.html
 app.use(helmet());
+//для работы с фронтендом оборачивается в гет-запрос.
+
+//передать из браузера нужный ключ после ответа на вопрос и найти по нему имена, записать в результат.
+//
+// mongoClient.connect('mongodb://localhost:27017/animals', function(err, db) {
+//     if (err) {
+//         throw err;
+//     }
+//
+//     //const collection = db.collection('furry');
+//
+//     animals.find({ keys: 'animalkey' }).toArray(function(err, result) {
+//         if (err) {
+//             throw err;
+//         }
+//
+//         console.log(result);
+//
+//     });
+//
+// });
 
 
-app.use(express.static(path.join(__dirname, './../frontend/build')));
 
-app.get('/animals', (req, res) => {
-    mongoClient.connect('mongodb://localhost:27017/animals', function(err, db) {
-        if (err) {
-            throw err;
-        }
 
-        const collection = db.collection('furry');
+//app.use(express.static(path.join(__dirname, './../frontend/build')));
+//гет-запрос для фронтенда.
+// app.get('./animals', (req, res) => {
+//     mongoClient.connect('mongodb://localhost:27017/animals', function(err, db) {
+//         if (err) {
+//             throw err;
+//         }
+//
+//         const collection = db.collection('furry');
+//
+//         collection.find({ name: 'богомол' }).toArray(function(err, result) {
+//             if (err) {
+//                 throw err;
+//             }
+//
+//             console.log(result);
+//
+//             res.json(result);
+//         });
+//
+//     });
+//
+// });
 
-        collection.find({ name: 'cat' }).toArray(function(err, result) {
-            if (err) {
-                throw err;
-            }
+app.get('/animals:name', (req, res) => {
+   console.log(res.send(req.params.name));
 
-            console.log(result);
+    
+});
+//получили из браузера некий параметр.
 
-            res.json(result);
-        });
+app.get('/animals/all', (req, res) => {
+    
 
-    });
+    console.log(res.send(req.params));
+});
 
+app.get('/questions', (req, res) => {
+    
+
+    console.log(res.send(req.params));
 });
 
 
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname , 'index.html'));
-});
-
+//app.get('/', (req, res) => {
+ // res.sendFile(path.join(__dirname , 'index.html'));
+//});
 
 
 
 
-const port = process.env.PORT || 5000;
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
   console.log(`Application example listening to port ${port}`);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+        text: 'text from the database'
+    }
+  }
+
+
+  //все обработчики должны быть внутри компонента надо использовать
+  // стрелочные ф-и, чтоб иметь возможность обратиться именно к этому компоненту через this
+  chooseForBase = (e) => {
+
+    console.log('event: ', e);
+    console.log("button pressed");
+
+    fetch("http://bla-bla.com").then((res) => {
+        console.log(res);
+
+        this.setState({text: res});
+    });
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,9 +36,18 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+          <button onClick={this.chooseForBase}>Choose</button>
+
+          <p>
+              Some default text: {this.state.text}
+          </p>
       </div>
     );
-  }
+
+
 }
+ 
+
 
 export default App;
