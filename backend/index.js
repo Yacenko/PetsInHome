56b5ddd6fdc7e0bc3d9ce9f24b2cc24168576d2e
Marina Yatsenko <mjacenk@gmail.com>
2017-12-26 17:15:45 +0200

    Create routes

--- backend/index.js
+++ backend/index.js
@@ -7,97 +7,85 @@ const app = express();
 
 const mongoClient = require('mongodb').MongoClient;
 
-// http://expressjs.com/uk/advanced/best-practice-security.html
 app.use(helmet());
 //для работы с фронтендом оборачивается в гет-запрос.
 
 //передать из браузера нужный ключ после ответа на вопрос и найти по нему имена, записать в результат.
-//
-// mongoClient.connect('mongodb://localhost:27017/animals', function(err, db) {
-//     if (err) {
-//         throw err;
-//     }
-//
-//     //const collection = db.collection('furry');
-//
-//     animals.find({ keys: 'animalkey' }).toArray(function(err, result) {
-//         if (err) {
-//             throw err;
-//         }
-//
-//         console.log(result);
-//
-//     });
-//
-// });
-
-
-
-
-//app.use(express.static(path.join(__dirname, './../frontend/build')));
-//гет-запрос для фронтенда.
-// app.get('./animals', (req, res) => {
-//     mongoClient.connect('mongodb://localhost:27017/animals', function(err, db) {
-//         if (err) {
-//             throw err;
-//         }
-//
-//         const collection = db.collection('furry');
-//
-//         collection.find({ name: 'богомол' }).toArray(function(err, result) {
-//             if (err) {
-//                 throw err;
-//             }
-//
-//             console.log(result);
-//
-//             res.json(result);
-//         });
-//
-//     });
-//
-// });
-
-app.get('/animals:name', (req, res) => {
-   console.log(res.send(req.params.name));
-
-    
-});
-//получили из браузера некий параметр.
 
 app.get('/animals/all', (req, res) => {
     
+  mongoClient.connect('mongodb://localhost:27017/animals', function(err, db) {
+        
+    if (err) {
+      throw err;
+    }
 
+    db.collection('zoo').find({}).toArray(function(err, result) {
+            
+      if (err) {
+        throw err;
+      }
+            
+      res.json(result);
+    });
 
-    console.log(res.send(req.params));
+  });
+  
+});
+
+app.get('/animals/:name', (req, res) => {
+   
+
+  mongoClient.connect('mongodb://localhost:27017/animals', function(err, db) {
+        
+    if (err) {
+      throw err;
+    }
+
+    let name = res.send(req.params.name);
+
+    // имя БД анималс передалось нам в параметре дб, далее надо работать с одной коллекцией
+    db.collection('zoo').find({ name }).toArray(function(err, result) {
+            
+      if (err) {
+        throw err;
+      }
+            
+      res.json(result);
+    });
+
+  });
+ 
 });
 
 
 app.get('/questions', (req, res) => {
     
+  mongoClient.connect('mongodb://localhost:27017/animals', function(err, db) {
+        
+    if (err) {
+      throw err;
+    }
 
-    console.log(res.send(req.params));
+    db.collection('questions').find().toArray(function(err, result) {
+            
+      if (err) {
+        throw err;
+      }
+            
+      res.json(result);
+    });
+
+  });
+    
 });
-
-
-//app.get('/', (req, res) => {
- // res.sendFile(path.join(__dirname , 'index.html'));
-//});
-
-
-
-
-
-const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname , 'index.html'));
 });
 
-
-const port = process.env.PORT || 5000;
-
+const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-  console.log(`Application example listening to port ${port}`);
+  console.log(`Application is listening to port ${port}`);
 });
