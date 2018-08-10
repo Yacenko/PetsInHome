b84229fe60216e63414ced26f76229d7a15e1ca6
Ivan Sidorenko <i.sidorenko@plarium.com>
2018-08-10 17:28:56 +0300

    Use different environments

--- frontend/src/utils/get-default-language.js
+++ frontend/src/utils/get-default-language.js
@@ -2,6 +2,7 @@
  * Gets current browser language
  */
 const getDefaultLanguage = () => {
+  console.log('Default environment language: ', process.env.REACT_APP_DEFAULT_LANGUAGE);
   return navigator.language.split('-')[0] || process.env.REACT_APP_DEFAULT_LANGUAGE;
 };
 
