86d35300d2fcbd71d3b3c0800e2303df28854b30
Ivan Sidorenko <i.sidorenko@plarium.com>
2018-08-10 11:44:07 +0300

    remove some changes

--- server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http'),
-    port = process.env.PORT || 8001;
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content_Type': 'text/plain'});
-    res.end('Hello, fucking world!\n');
-}).listen(parseInt(port));
